1dd7659932c034f2903d72a63c87798e3c153676
Mubarak <mubee2004@gmail.com>
2023-11-26 12:45:37 +0100

    fixed broken links issue

--- how/j.js
+++ how/j.js
@@ -1,11 +1,11 @@
 // Slide through images every 20 minutes
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll(".slide");
 
 function showSlide() {
-  slides.forEach(slide => (slide.style.display = 'none'));
+  slides.forEach((slide) => (slide.style.display = "none"));
   currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].style.display = 'block';
+  slides[currentSlide].style.display = "block";
 }
 
 setInterval(showSlide, 1000); // 20 minutes
@@ -13,14 +13,14 @@ setInterval(showSlide, 1000); // 20 minutes
 // Dynamic question display
 // JavaScript to control the quiz
 
-document.addEventListener('DOMContentLoaded', function () {
-  const questions = document.querySelectorAll('.question');
+document.addEventListener("DOMContentLoaded", function () {
+  const questions = document.querySelectorAll(".question");
   let currentQuestion = 0;
   let correctAnswers = 0;
 
   function showQuestion(index) {
-    questions.forEach(question => question.style.display = 'none');
-    questions[index].style.display = 'block';
+    questions.forEach((question) => (question.style.display = "none"));
+    questions[index].style.display = "block";
   }
 
   function validateAnswers() {
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const radios = questions[i].querySelectorAll('input[type="radio"]');
       let answered = false;
 
-      radios.forEach(radio => {
+      radios.forEach((radio) => {
         if (radio.checked) {
           answered = true;
           userAnswers.push(radio.value);
@@ -51,11 +51,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function getCorrectAnswer(questionIndex) {
     const correctAnswersMap = {
-      0: 'c',
-      1: 'b',
-      2: 'c',
-      3: 'c',
-      4: 'c'
+      0: "c",
+      1: "b",
+      2: "c",
+      3: "c",
+      4: "c",
     };
     return correctAnswersMap[questionIndex];
   }
@@ -63,19 +63,21 @@ document.addEventListener('DOMContentLoaded', function () {
   function showResultPage() {
     const userAnswers = validateAnswers();
     if (correctAnswers === questions.length) {
-      alert('Congratulations! You answered all questions correctly!');
-      
-      window.location.href = '/main/how/how.html'; // Redirect to the success page
+      alert("Congratulations! You answered all questions correctly!");
+
+      window.location.href = "how.html"; // Redirect to the success page
     } else {
-      alert('Oops! You did not answer all questions correctly. Please retake the quiz.');
+      alert(
+        "Oops! You did not answer all questions correctly. Please retake the quiz."
+      );
       currentQuestion = 0;
       correctAnswers = 0;
       showQuestion(currentQuestion);
     }
   }
 
-  document.querySelectorAll('.next').forEach(button => {
-    button.addEventListener('click', function () {
+  document.querySelectorAll(".next").forEach((button) => {
+    button.addEventListener("click", function () {
       if (currentQuestion < questions.length - 1) {
         currentQuestion++;
         showQuestion(currentQuestion);
@@ -83,8 +85,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  document.querySelectorAll('.prev').forEach(button => {
-    button.addEventListener('click', function () {
+  document.querySelectorAll(".prev").forEach((button) => {
+    button.addEventListener("click", function () {
       if (currentQuestion > 0) {
         currentQuestion--;
         showQuestion(currentQuestion);
@@ -92,10 +94,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  document.getElementById('quizForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent default form submission behavior
-    showResultPage();
-  });
+  document
+    .getElementById("quizForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent default form submission behavior
+      showResultPage();
+    });
 
   showQuestion(currentQuestion);
 });
